134bcc4991278b1137b9282555041448814a3675
Dat Truong <dattruong@Dats-MacBook-Pro.local>
2023-04-15 22:41:16 +0700

    use cachednetworkimage

--- lib/widgets/recommended_tutor_card.dart
+++ lib/widgets/recommended_tutor_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../core/assets/index.dart';
@@ -39,24 +40,24 @@ class RecommendedTutorCardWidget extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Container(
-                        margin: const EdgeInsets.only(right: 10, top: 10),
-                        child: CircleAvatar(
-                          backgroundColor: Colors.transparent,
-                          radius: 30,
-                          child: avatar == null
-                              ? Image.asset(
-                                  LetTutorImages.avatar,
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.network(
-                                  avatar as String,
-                                  errorBuilder: (context, error, stackTrace) =>
-                                      Image.asset(
-                                    LetTutorImages.avatar,
-                                    fit: BoxFit.cover,
-                                  ),
-                                  fit: BoxFit.cover,
-                                ),
+                        margin: const EdgeInsets.only(bottom: 10, right: 10),
+                        height: 60,
+                        width: 60,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: CachedNetworkImage(
+                            imageUrl: avatar ?? 'https://picsum.photos/200/300',
+                            fit: BoxFit.cover,
+                            width: double.maxFinite,
+                            height: double.maxFinite,
+                            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                                CircularProgressIndicator(
+                                    value: downloadProgress.progress),
+                            errorWidget: (context, url, error) => Image.asset(
+                              LetTutorImages.avatar,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       ),
                       Expanded(
